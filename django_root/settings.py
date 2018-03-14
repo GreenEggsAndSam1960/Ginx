ff5c7a76f49a7988e220af328e41a9514cf837c2
samakj <samakj@live.co.uk>
2018-03-14 21:07:17 +0000

    Modify django settings file to use react frontend
     - Static assets are kept in the react_root
     - Webpack loader can be used to import react app

--- django_root/settings.py
+++ django_root/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'webpack_loader',
+    'rest_framework',
+
+    'django_root',
+    'react_root',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +121,27 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'react_root', 'static')
+
+# Setting up media roots
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'react_root', 'media')
+MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'Frontend/static/jsbundles/local/',  # end with slash
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-local.json'),
+    }
+}

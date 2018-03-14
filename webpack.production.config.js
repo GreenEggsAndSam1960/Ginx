69a813546b8e418c5f8e7ed0685ea93b26fd2a7f
samakj <samakj@live.co.uk>
2018-03-14 21:04:12 +0000

    Initiate a basic hello world react app
     - Includes:
        - Hot reloading server
        - Sass compilation
        - Config files for local, staging and server
        - An index template that returns Hello world

---
+++ webpack.production.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+var config = require('./webpack.base.config.js')
+
+config.output.path = require('path').resolve('./Frontend/static/js/bundles/prod/')
+
+config.plugins = config.plugins.concat([
+  new BundleTracker({filename: './webpack-stats-prod.json'}),
+
+  // removes a lot of debugging code in React
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production'),
+      'BASE_API_URL': JSON.stringify('https://example.com/api/v1/'),
+  }}),
+
+  // keeps hashes consistent between compilations
+  new webpack.optimize.OccurenceOrderPlugin(),
+
+  // minifies your code
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false
+    }
+  })
+])
+
+module.exports = config
